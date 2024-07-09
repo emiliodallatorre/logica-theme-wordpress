0f3bd316a30ae467146582af3e28608a84e7985c
Leo G <leogazz02@gmail.com>
2024-07-09 22:17:12 +0200

    Add footer.css

--- functions.php
+++ functions.php
@@ -7,5 +7,21 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+<<<<<<< Updated upstream
+=======
+
+    // Define an array of stylesheet components to enqueue.
+    $component_styles = ['header', 'footer'];
+
+    // Loop through each component and enqueue its corresponding stylesheet.
+    foreach ($component_styles as $component) {
+        wp_enqueue_style(
+            "child-{$component}-style",
+            get_stylesheet_directory_uri() . "/styles/{$component}.css",
+            array($main_style), // Make sure to list the main child theme stylesheet as a dependency.
+            wp_get_theme()->get('Version')
+        );
+    }
+>>>>>>> Stashed changes
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
