a3355ff22961c25e703ca2b0cc170ab4148154ee
Emilio Dalla Torre <emilio.dalla.torre@pwc.com>
2024-07-09 19:45:30 +0200

    Rewrote CSS hierarchy

--- functions.php
+++ functions.php
@@ -1,11 +1,30 @@
 <?php
-function my_theme_enqueue_styles() {
-    $parent_style = 'davis-blocks-style'; // This is 'parent-style' for the Twenty Seventeen theme.
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
+function theme_enqueue_styles()
+{
+    $parent_style = 'parent-style';
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+
+    // Enqueue the child theme stylesheet.
+    $main_style = 'child-style';
+    wp_enqueue_style(
+        $main_style,
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array($parent_style),
         wp_get_theme()->get('Version')
     );
+
+    // Define an array of stylesheet components to enqueue.
+    $component_styles = ['header']; //, 'footer'];
+
+    // Loop through each component and enqueue its corresponding stylesheet.
+    foreach ($component_styles as $component) {
+        wp_enqueue_style(
+            "child-{$component}-style",
+            get_stylesheet_directory_uri() . "/styles/{$component}.css",
+            array($main_style), // Make sure to list the main child theme stylesheet as a dependency.
+            wp_get_theme()->get('Version')
+        );
+    }
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
